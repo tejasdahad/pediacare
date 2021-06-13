6e506fd0011a0409c0d23cbd5fea92b58b325a09
Tejas Dahad <tejasdahad000@gmail.com>
2021-06-13 09:31:05 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+import { Navigation } from './components/navigation'
+import { Header } from './components/header'
+import { Features } from './components/features'
+import { About } from './components/about'
+import { Services } from './components/services'
+import { Gallery } from './components/gallery'
+import { Contact } from './components/contact'
+import JsonData from './data/data.json'
+import SmoothScroll from 'smooth-scroll'
+
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+})
+
+const App = () => {
+  const [landingPageData, setLandingPageData] = useState({})
+  useEffect(() => {
+    setLandingPageData(JsonData)
+  }, [])
+
+  return (
+    <div>
+      <Navigation />
+      <Header data={landingPageData.Header} />
+      <Features data={landingPageData.Features} />
+      <About data={landingPageData.About} />
+      <Services data={landingPageData.Services} />
+      <Gallery />
+      <Contact data={landingPageData.Contact} />
+    </div>
+  )
+}
+
+export default App
